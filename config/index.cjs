0e86420f261e957afc9ad718e9392cef80d57296
Rebor686 <108292359+Rebor686@users.noreply.github.com>
2022-10-28 16:40:00 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -32,19 +32,19 @@ const USER_CONFIG = {
           type: '*生日', name: '宝贝', year: '2001', date: '10-05',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
-        {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
-        },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+//         {
+//           type: '生日', name: '李四', year: '1996', date: '09-31',
+//         },
+//         {
+//           type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+//         },
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2020-12-25' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+//         { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
