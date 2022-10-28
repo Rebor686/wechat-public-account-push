a37e427e167710db33e645f26a0216b17e528d45
Rebor686 <108292359+Rebor686@users.noreply.github.com>
2022-10-28 16:53:12 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'osNW45szOECdnfkhFeRJTFRjG0MM',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'gwl27xjxPHqPQSsi4PpEAPDKZl4WS9Y278-kP88eHzo',
+      useTemplateId: 'H3mEUq-KfsQdw5hGuYASfQyVDeq-qCjmkSBe0SBbEGg',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '10-29',
       festivals: [
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'gwl27xjxPHqPQSsi4PpEAPDKZl4WS9Y278-kP88eHzo',
+  CALLBACK_TEMPLATE_ID: 'H3mEUq-KfsQdw5hGuYASfQyVDeq-qCjmkSBe0SBbEGg',
 
   CALLBACK_USERS: [
     {
